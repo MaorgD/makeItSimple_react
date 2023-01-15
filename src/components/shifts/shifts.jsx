d82ee5c7288595659712302fc61436d0e509b368
benshirom <57137329+benshirom@users.noreply.github.com>
2023-01-16 00:27:53 +0200

    add shift calendar

--- src/components/shifts/shifts.jsx
+++ src/components/shifts/shifts.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom';
 import ShiftTable from './shiftTable';
 import { useSelector, useDispatch } from 'react-redux';
@@ -7,29 +7,32 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { TouchBackend } from 'react-dnd-touch-backend'
 import { WorkerBox } from './workerBox'
+import { ShiftCalendar } from './shiftCalendar';
 
 const Shifts = () => {
     const { restaurant } = useSelector((state) => state.restaurantSlice);
     const { user } = useSelector((state) => state.userSlice);
-    const today = new Date();
-    const [days, setDays] = useState(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
-    const [typeShifts, setTypeShifts] = useState([{ type: 'Morning', hours: "10:00-13:00" },{ type: 'Noon', hours: "13:00-16:00" }, { type: 'Afternoon', hours: "16:00-19:00" }, { type: 'Evening', hours: "19:00-23:00" }])
+    // const today = new Date();
+    // const [days, setDays] = useState(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
+    // const [typeShifts, setTypeShifts] = useState([{ type: 'Morning', hours: "10:00-13:00" }, { type: 'Noon', hours: "13:00-16:00" }, { type: 'Afternoon', hours: "16:00-19:00" }, { type: 'Evening', hours: "19:00-23:00" }])
     const [isEditShifts, setIsEditShifts] = useState(false);
-    console.log(days[today.getDay()])
+    const [shifts, setShifts] = useState([]);
+    const selectWorkerRef = useRef();
 
-    const handleDragStart = (event, employeeId) => {
-        event.dataTransfer.setData('employeeId', employeeId);
-    };
-    const detectDeviceType = () =>
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-      ? 'Mobile'
-      : 'Desktop';
-
+    
+    // const handleDragStart = (event, employeeId) => {
+    //     event.dataTransfer.setData('employeeId', employeeId);
+    // };
+    // const detectDeviceType = () =>
+    // /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    //   ? 'Mobile'
+    //   : 'Desktop';
+    
     return (
         <>
-            <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-                <div className='flex-col mx-0'>
-                    <div className='flex-col space-y-3  mb-3 text-center'>
+            <div className=" min-h-full items-center justify-center py-12 ">
+                <div className=''>
+                    {user?.data?.worker?.jobs.includes("manager")&&<div className='flex-col space-y-3  mb-3 text-center'>
 
                         <Link className='bg-indigo-400 rounded-full p-1  mr-1' to={"/manager/workers"} >check workers list </Link>
                         <button
@@ -40,14 +43,17 @@ const Shifts = () => {
                                 else
                                     setIsEditShifts(true)
                             }}>edit shifts</button>
-                        {user?.data?.worker?.jobs.includes("manager") && isEditShifts && <div className=" flex justify-evenly mb-6 ">
-                            <ShiftControl days={days} setDays={setDays} setTypeShifts={setTypeShifts} workers={restaurant?.workersArray} />
 
-                        </div>}
-                    </div>
+                        {isEditShifts && <select ref={selectWorkerRef} name="employees" id="employees">
+                            <option key={"select worker"} value={null}>select worker</option>
+                            {restaurant?.workersArray.map((employee) => (<option key={employee._id} id={employee._id} value={employee._id}>{employee.fullName.firstName} {employee.fullName.lastName} </option>))}
+                        </select>}
 
 
-                    <DndProvider backend={detectDeviceType() == 'Mobile' ? TouchBackend : HTML5Backend}>
+                    </div>}
+
+                    <ShiftCalendar employees={restaurant?.workersArray} isEditShifts={isEditShifts} selectWorkerRef={selectWorkerRef}  shifts={shifts} />
+                    {/* <DndProvider backend={detectDeviceType() == 'Mobile' ? TouchBackend : HTML5Backend}>
                         <div className="" >
                             <ShiftTable days={days} typeShifts={typeShifts} shifts={restaurant?.shifts} workers={restaurant?.workersArray} isEditShifts={isEditShifts} />
                         </div>
@@ -61,7 +67,7 @@ const Shifts = () => {
                                     })}
                                 </div>}
                         
-                    </DndProvider>
+                    </DndProvider> */}
 
 
 
