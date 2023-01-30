d8e580b89224b8a76671e64bf0176c6076e97e65
Maor <maorgabaynew@gmail.com>
2023-01-30 13:42:45 +0200

    start orders

---
+++ src/components/orderMenu/dropItems.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DropItems = () => {
+  return (
+    <div>DropItems</div>
+  )
+}
+
+export default DropItems
---
+++ src/components/orderMenu/menuOrderNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const MenuOrderNav = (props) => {
+
+  return (
+    <div>MenuOrderNav</div>
+  )
+}
+
+export default MenuOrderNav
